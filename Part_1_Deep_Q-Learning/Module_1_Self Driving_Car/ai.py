707b505ec1d0e9fb3f8fd3edbe05e9cffae6f5f8
Tuatini Godard <tuatinigodard@gmail.com>
2017-07-24 16:14:31 +0200

    Update DQN code

--- Part_1_Deep_Q-Learning/Module_1_Self Driving_Car/ai.py
+++ Part_1_Deep_Q-Learning/Module_1_Self Driving_Car/ai.py
@@ -12,70 +12,102 @@ import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
 
+
 # Creating the architecture of the Neural Network
 
 class Network(nn.Module):
-    
     def __init__(self, input_size, nb_action):
         super(Network, self).__init__()
         self.input_size = input_size
         self.nb_action = nb_action
         self.fc1 = nn.Linear(input_size, 30)
         self.fc2 = nn.Linear(30, nb_action)
-    
+
     def forward(self, state):
         x = F.relu(self.fc1(state))
         q_values = self.fc2(x)
         return q_values
 
+
 # Implementing Experience Replay
 
 class ReplayMemory(object):
-    
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
-    
+
     def push(self, event):
         self.memory.append(event)
         if len(self.memory) > self.capacity:
             del self.memory[0]
-    
+
     def sample(self, batch_size):
+        # (state1, state2), (action1, action2), (reward1, reward2)...
         samples = zip(*random.sample(self.memory, batch_size))
+        # torch.cat aligns everything as (state, action, reward)
         return map(lambda x: Variable(torch.cat(x, 0)), samples)
+
 
 # Implementing Deep Q Learning
 
 class Dqn():
-    
     def __init__(self, input_size, nb_action, gamma):
         self.gamma = gamma
         self.reward_window = []
         self.model = Network(input_size, nb_action)
         self.memory = ReplayMemory(100000)
-        self.optimizer = optim.Adam(self.model.parameters(), lr = 0.001)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+        # Vector of dimension 6. The 3 signals of the 3 sensors and orientation/ - orientation
+        # (batch_size, left, straight, right, orientation, -orientation)
+        # batch_size dimension is added with unsqueeze
         self.last_state = torch.Tensor(input_size).unsqueeze(0)
+        # 0, 1 or 2 (indexed), see the var action2rotation in map.py
         self.last_action = 0
         self.last_reward = 0
-    
+
     def select_action(self, state):
-        probs = F.softmax(self.model(Variable(state, volatile = True))*100) # T=100
+        """
+        Take the input state (the 3 Q-values) and return the best possible action
+        :param state: The input state of the neural net (left, straight, right, orientation, -orientation)
+        :return: The best possible action probabilities
+        """
+        # volatile = True : We don't want the gradient in the graph of all the computation of the nn module
+        # 100 is the temperature parameter or the certainty about the next action to play
+        # The closer it is to 0 the less sure the nn will be
+        # to take the action. Far from 0, the more sure it will be about the action to play
+        # ex with T = 3: softmax([0.04, 0.11, 0.85]) => softmax([1,2,3] * 3) = [0, 0.02, 0.98]
+        probs = F.softmax(self.model(Variable(state, volatile=True)) * 100)  # T=100
+        # random draw from the probabilities
         action = probs.multinomial()
-        return action.data[0,0]
-    
+        # Retrieve the action at index [0, 0]
+        return action.data[0, 0]
+
     def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
+        """
+        Train the nn
+        :param batch_state:
+        :param batch_next_state:
+        :param batch_reward:
+        :param batch_action:
+        :return:
+        """
         outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
         next_outputs = self.model(batch_next_state).detach().max(1)[0]
-        target = self.gamma*next_outputs + batch_reward
+        target = self.gamma * next_outputs + batch_reward
+        # td = temporal difference
         td_loss = F.smooth_l1_loss(outputs, target)
+        # Reinitialize the Adam optimizer from the constructor
         self.optimizer.zero_grad()
-        td_loss.backward(retain_variables = True)
+        # Backprop, retain_variables=True to free the memory
+        td_loss.backward(retain_variables=True)
         self.optimizer.step()
-    
+
     def update(self, reward, new_signal):
+        # The new states are the current signals
         new_state = torch.Tensor(new_signal).float().unsqueeze(0)
-        self.memory.push((self.last_state, new_state, torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward])))
+        self.memory.push(
+            (self.last_state, new_state,
+             torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward])))
         action = self.select_action(new_state)
         if len(self.memory.memory) > 100:
             batch_state, batch_next_state, batch_action, batch_reward = self.memory.sample(100)
@@ -87,15 +119,16 @@ class Dqn():
         if len(self.reward_window) > 1000:
             del self.reward_window[0]
         return action
-    
+
     def score(self):
-        return sum(self.reward_window)/(len(self.reward_window)+1.)
-    
+        # +1 to avoid dividing by 0
+        return sum(self.reward_window) / (len(self.reward_window) + 1.)
+
     def save(self):
         torch.save({'state_dict': self.model.state_dict(),
-                    'optimizer' : self.optimizer.state_dict(),
-                   }, 'last_brain.pth')
-    
+                    'optimizer': self.optimizer.state_dict(),
+                    }, 'last_brain.pth')
+
     def load(self):
         if os.path.isfile('last_brain.pth'):
             print("=> loading checkpoint... ")
--- Part_1_Deep_Q-Learning/Module_1_Self Driving_Car/map.py
+++ Part_1_Deep_Q-Learning/Module_1_Self Driving_Car/map.py
@@ -29,30 +29,33 @@ n_points = 0
 length = 0
 
 # Getting our AI, which we call "brain", and that contains our neural network that represents our Q-function
-brain = Dqn(5,3,0.9)
-action2rotation = [0,20,-20]
+brain = Dqn(5, 3, 0.9)
+action2rotation = [0, 20, -20]
 last_reward = 0
 scores = []
 
 # Initializing the map
 first_update = True
+
+
 def init():
     global sand
     global goal_x
     global goal_y
     global first_update
-    sand = np.zeros((longueur,largeur))
+    sand = np.zeros((longueur, largeur))
     goal_x = 20
     goal_y = largeur - 20
     first_update = False
 
+
 # Initializing the last distance
 last_distance = 0
+
 
 # Creating the car class
 
 class Car(Widget):
-    
     angle = NumericProperty(0)
     rotation = NumericProperty(0)
     velocity_x = NumericProperty(0)
@@ -75,30 +78,39 @@ class Car(Widget):
         self.pos = Vector(*self.velocity) + self.pos
         self.rotation = rotation
         self.angle = self.angle + self.rotation
+        # (the sensors are the 3 colored squares in front of the car)
         self.sensor1 = Vector(30, 0).rotate(self.angle) + self.pos
-        self.sensor2 = Vector(30, 0).rotate((self.angle+30)%360) + self.pos
-        self.sensor3 = Vector(30, 0).rotate((self.angle-30)%360) + self.pos
-        self.signal1 = int(np.sum(sand[int(self.sensor1_x)-10:int(self.sensor1_x)+10, int(self.sensor1_y)-10:int(self.sensor1_y)+10]))/400.
-        self.signal2 = int(np.sum(sand[int(self.sensor2_x)-10:int(self.sensor2_x)+10, int(self.sensor2_y)-10:int(self.sensor2_y)+10]))/400.
-        self.signal3 = int(np.sum(sand[int(self.sensor3_x)-10:int(self.sensor3_x)+10, int(self.sensor3_y)-10:int(self.sensor3_y)+10]))/400.
-        if self.sensor1_x>longueur-10 or self.sensor1_x<10 or self.sensor1_y>largeur-10 or self.sensor1_y<10:
+        self.sensor2 = Vector(30, 0).rotate((self.angle + 30) % 360) + self.pos
+        self.sensor3 = Vector(30, 0).rotate((self.angle - 30) % 360) + self.pos
+        self.signal1 = int(np.sum(sand[int(self.sensor1_x) - 10:int(self.sensor1_x) + 10,
+                                  int(self.sensor1_y) - 10:int(self.sensor1_y) + 10])) / 400.
+        self.signal2 = int(np.sum(sand[int(self.sensor2_x) - 10:int(self.sensor2_x) + 10,
+                                  int(self.sensor2_y) - 10:int(self.sensor2_y) + 10])) / 400.
+        self.signal3 = int(np.sum(sand[int(self.sensor3_x) - 10:int(self.sensor3_x) + 10,
+                                  int(self.sensor3_y) - 10:int(self.sensor3_y) + 10])) / 400.
+        if self.sensor1_x > longueur - 10 or self.sensor1_x < 10 or self.sensor1_y > largeur - 10 or self.sensor1_y < 10:
             self.signal1 = 1.
-        if self.sensor2_x>longueur-10 or self.sensor2_x<10 or self.sensor2_y>largeur-10 or self.sensor2_y<10:
+        if self.sensor2_x > longueur - 10 or self.sensor2_x < 10 or self.sensor2_y > largeur - 10 or self.sensor2_y < 10:
             self.signal2 = 1.
-        if self.sensor3_x>longueur-10 or self.sensor3_x<10 or self.sensor3_y>largeur-10 or self.sensor3_y<10:
+        if self.sensor3_x > longueur - 10 or self.sensor3_x < 10 or self.sensor3_y > largeur - 10 or self.sensor3_y < 10:
             self.signal3 = 1.
+
 
 class Ball1(Widget):
     pass
+
+
 class Ball2(Widget):
     pass
+
+
 class Ball3(Widget):
     pass
+
 
 # Creating the game class
 
 class Game(Widget):
-
     car = ObjectProperty(None)
     ball1 = ObjectProperty(None)
     ball2 = ObjectProperty(None)
@@ -126,21 +138,21 @@ class Game(Widget):
 
         xx = goal_x - self.car.x
         yy = goal_y - self.car.y
-        orientation = Vector(*self.car.velocity).angle((xx,yy))/180.
+        orientation = Vector(*self.car.velocity).angle((xx, yy)) / 180.
         last_signal = [self.car.signal1, self.car.signal2, self.car.signal3, orientation, -orientation]
         action = brain.update(last_reward, last_signal)
         scores.append(brain.score())
         rotation = action2rotation[action]
         self.car.move(rotation)
-        distance = np.sqrt((self.car.x - goal_x)**2 + (self.car.y - goal_y)**2)
+        distance = np.sqrt((self.car.x - goal_x) ** 2 + (self.car.y - goal_y) ** 2)
         self.ball1.pos = self.car.sensor1
         self.ball2.pos = self.car.sensor2
         self.ball3.pos = self.car.sensor3
 
-        if sand[int(self.car.x),int(self.car.y)] > 0:
+        if sand[int(self.car.x), int(self.car.y)] > 0:
             self.car.velocity = Vector(1, 0).rotate(self.car.angle)
             last_reward = -1
-        else: # otherwise
+        else:  # otherwise
             self.car.velocity = Vector(6, 0).rotate(self.car.angle)
             last_reward = -0.2
             if distance < last_distance:
@@ -159,26 +171,27 @@ class Game(Widget):
             self.car.y = self.height - 10
             last_reward = -1
 
+        # Updates the goal when the goal is reached (upper left corner to bottom right, from airport to downtown)
         if distance < 100:
-            goal_x = self.width-goal_x
-            goal_y = self.height-goal_y
+            goal_x = self.width - goal_x
+            goal_y = self.height - goal_y
         last_distance = distance
+
 
 # Adding the painting tools
 
 class MyPaintWidget(Widget):
-
     def on_touch_down(self, touch):
         global length, n_points, last_x, last_y
         with self.canvas:
-            Color(0.8,0.7,0)
+            Color(0.8, 0.7, 0)
             d = 10.
-            touch.ud['line'] = Line(points = (touch.x, touch.y), width = 10)
+            touch.ud['line'] = Line(points=(touch.x, touch.y), width=10)
             last_x = int(touch.x)
             last_y = int(touch.y)
             n_points = 0
             length = 0
-            sand[int(touch.x),int(touch.y)] = 1
+            sand[int(touch.x), int(touch.y)] = 1
 
     def on_touch_move(self, touch):
         global length, n_points, last_x, last_y
@@ -186,29 +199,29 @@ class MyPaintWidget(Widget):
             touch.ud['line'].points += [touch.x, touch.y]
             x = int(touch.x)
             y = int(touch.y)
-            length += np.sqrt(max((x - last_x)**2 + (y - last_y)**2, 2))
+            length += np.sqrt(max((x - last_x) ** 2 + (y - last_y) ** 2, 2))
             n_points += 1.
-            density = n_points/(length)
+            density = n_points / (length)
             touch.ud['line'].width = int(20 * density + 1)
-            sand[int(touch.x) - 10 : int(touch.x) + 10, int(touch.y) - 10 : int(touch.y) + 10] = 1
+            sand[int(touch.x) - 10: int(touch.x) + 10, int(touch.y) - 10: int(touch.y) + 10] = 1
             last_x = x
             last_y = y
+
 
 # Adding the API Buttons (clear, save and load)
 
 class CarApp(App):
-
     def build(self):
         parent = Game()
         parent.serve_car()
-        Clock.schedule_interval(parent.update, 1.0/60.0)
+        Clock.schedule_interval(parent.update, 1.0 / 60.0)
         self.painter = MyPaintWidget()
-        clearbtn = Button(text = 'clear')
-        savebtn = Button(text = 'save', pos = (parent.width, 0))
-        loadbtn = Button(text = 'load', pos = (2 * parent.width, 0))
-        clearbtn.bind(on_release = self.clear_canvas)
-        savebtn.bind(on_release = self.save)
-        loadbtn.bind(on_release = self.load)
+        clearbtn = Button(text='clear')
+        savebtn = Button(text='save', pos=(parent.width, 0))
+        loadbtn = Button(text='load', pos=(2 * parent.width, 0))
+        clearbtn.bind(on_release=self.clear_canvas)
+        savebtn.bind(on_release=self.save)
+        loadbtn.bind(on_release=self.load)
         parent.add_widget(self.painter)
         parent.add_widget(clearbtn)
         parent.add_widget(savebtn)
@@ -218,7 +231,7 @@ class CarApp(App):
     def clear_canvas(self, obj):
         global sand
         self.painter.canvas.clear()
-        sand = np.zeros((longueur,largeur))
+        sand = np.zeros((longueur, largeur))
 
     def save(self, obj):
         print("saving brain...")
@@ -229,6 +242,7 @@ class CarApp(App):
     def load(self, obj):
         print("loading last saved brain...")
         brain.load()
+
 
 # Running the whole thing
 if __name__ == '__main__':
